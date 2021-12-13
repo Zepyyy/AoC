5b9416b3e635a4f0e349920c72c786fe4723e578
Quentin <quentin.stubecki@viacesi.fr>
2021-12-14 00:11:18 +0100

    added 2.1

---
+++ AoC2.1.py
@@ -0,0 +1,16 @@
+liste = [
+    str(i)
+    for i in open("C:\\users\\quent\\Desktop\\AoC2021\\AoC2.txt", "r").readlines()
+]
+horizontal = 0
+depths = 0
+
+for i in liste:
+    if i[0:2] == "up":
+        depths -= int(i[3])
+    elif i[0:4] == "down":
+        depths += int(i[5])
+    elif i[0:7] == "forward":
+        horizontal += int(i[8])
+
+print(horizontal * depths)
