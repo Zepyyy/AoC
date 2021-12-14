6b1f41be090bb3dcc1aa38bcac64e961bed9760b
Quentin <quentin.stubecki@viacesi.fr>
2021-12-14 17:37:04 +0100

    changed 14

--- AoC14.1_.py
+++ AoC14.1_.py
@@ -2,18 +2,15 @@ liste = [
     str(i)
     for i in open("C:\\users\\quent\\Desktop\\AoC2021\\AoC14.txt", "r").readlines()
 ]
-
-polymer = liste[0]
-table = liste[2:]
-
+paire = [str(i)[6] for i in liste[2:]]
+elem = [str(i)[:2] for i in liste[2:]]
+polymer = [liste[0].replace("\n", "")]
 jour = 5
 
-print(polymer)
-print(table)
+print("polymer: ", polymer)
+print("paire: ", paire)
+print("elem: ", elem)
+
 
 for i in range(jour):
-    for j, l in enumerate(polymer):
-        for k in table:
-            print(k[:2])
-            print(k[6])
     print("jour: ", i + 1)
