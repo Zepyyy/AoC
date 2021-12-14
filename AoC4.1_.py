9f18a4e9616ed27b5344e118f2dd633bd278f266
Quentin <quentin.stubecki@viacesi.fr>
2021-12-14 11:58:14 +0100

    oui

--- AoC4.1_.py
+++ AoC4.1_.py
@@ -1,15 +1,11 @@
 liste = open("C:\\users\\quent\\Desktop\\AoC2021\\AoC4.txt", "r").readlines()
-
+liste1 = []
 a = str(liste[0]).split(",")
 b = str(liste[1:]).split(",")
 n = 0
 
-for i in a:
-    if n < 4:
-        print(i)
-        if i in b[n]:
-            print("y")
-        n += 1
-    else:
-        print()
-        n = 0
+for i in range(len(b) - 1):
+    if i % 5 == 0:
+        liste1.append(b[i])
+
+print(liste1)
