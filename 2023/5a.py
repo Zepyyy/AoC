ecc2393480c81c93249fc641a83c02715dac7e2c
Zepyyy <quentin.stubecki@viacesi.fr>
2023-12-05 17:56:55 +0100

    5

---
+++ 2023/5a.py
@@ -0,0 +1,68 @@
+def start():
+    with open('5.txt') as f:
+        input = f.read()
+    return input
+
+
+def map(seeds, *category):
+    locations = []
+    categories = ["soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
+    for seed in seeds:
+        # print(f"seed: {seed}")
+        value = seed
+        for i in category:
+            # print(f"category: {categories[int(category.index(i))]}")
+            # print(f"i: {i}")
+            for index,j in enumerate(i):
+                # print(f"j: {j}")
+                if int(j[1]) <= int(value) <= int(j[1])+int(j[2]):
+                    # print(f"value: {value}")
+                    # print(f"j[0]: {j[0]}")
+                    # print(f"j[1]: {j[1]}")
+                    # print(f"j[2]: {j[2]}")
+                    # print(f"value-j[1]: {int(value)-int(j[1])}")
+                    # print(f"yay: {int(j[0])+(int(value)-int(j[1]))}")
+                    value = int(j[0])+(int(value)-int(j[1]))
+                    # print(f"value: {value}")
+                    
+                    break
+                elif index != len(i)-1:
+                    # print("...............")
+                    # print(f"value: {value}")
+                    
+                    continue
+                else:
+                    value=value
+                    # print(f"value: {value}")
+                
+                    break
+        locations.append(value)
+    return min(locations)
+
+def ex(input):
+    import re
+    
+    # Use regular expressions to find matches in the entire text
+    seeds = re.findall(r'seeds: ([\d\s]+)', input)
+    soil = re.findall(r'seed-to-soil map:\n([\d\s\n]+)', input)
+    fertilizer = re.findall(r'soil-to-fertilizer map:\n([\d\s\n]+)', input)
+    water = re.findall(r'fertilizer-to-water map:\n([\d\s\n]+)', input)
+    light = re.findall(r'water-to-light map:\n([\d\s\n]+)', input)
+    temperature = re.findall(r'light-to-temperature map:\n([\d\s\n]+)', input)
+    humidity = re.findall(r'temperature-to-humidity map:\n([\d\s\n]+)', input)
+    location = re.findall(r'humidity-to-location map:\n([\d\s\n]+)', input)
+    
+    # Extract individual numbers from the captured groups
+    seeds = re.findall(r'\b\d+\b', ' '.join(seeds))
+    soil = [re.findall(r'\b\d+\b', line) for line in soil[0].split('\n') if line.strip()]
+    fertilizer = [re.findall(r'\b\d+\b', line) for line in fertilizer[0].split('\n') if line.strip()]
+    water = [re.findall(r'\b\d+\b', line) for line in water[0].split('\n') if line.strip()]
+    light = [re.findall(r'\b\d+\b', line) for line in light[0].split('\n') if line.strip()]
+    temperature = [re.findall(r'\b\d+\b', line) for line in temperature[0].split('\n') if line.strip()]
+    humidity = [re.findall(r'\b\d+\b', line) for line in humidity[0].split('\n') if line.strip()]
+    location = [re.findall(r'\b\d+\b', line) for line in location[0].split('\n') if line.strip()]
+
+    return map(seeds, soil, fertilizer, water, light, temperature, humidity, location)
+    # Add similar print statements for other variables if needed
+
+print(ex(start()))
