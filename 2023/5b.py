bf470274b317250ad54f0ff640244d87ff098f75
Quentin <quentin.stubecki@viacesi.fr>
2023-12-09 05:12:52 +0100

    7a + 5b

---
+++ 2023/5b.py
@@ -0,0 +1,66 @@
+def start():
+    with open('5.txt') as f:
+        input = f.read()
+    return input
+
+
+def map(seeds, *category):
+    locations = []
+    for couple in seeds[::2]:
+        size = seeds[seeds.index(couple)+1]
+        seedss = [i for i in range(int(couple), int(couple) + int(size)+1)]
+        for seed in seedss:
+            value = seed
+            for i in category:
+                for index, j in enumerate(i):
+                    if int(j[1]) <= int(value) <= int(j[1])+int(j[2]):
+                        value = int(j[0])+(int(value)-int(j[1]))
+                        break
+                    elif index != len(i)-1:
+                        continue
+                    else:
+                        value = value
+                        break
+            # print(f"value: {value}, seed: {seed}")
+            locations.append(value)
+    print(locations.index(min(locations))+1)
+    print([i for i in range(len(locations)) if locations[i] == min(locations)])
+    print(locations)
+    return min(locations)
+
+
+def ex(input):
+    import re
+
+    # Use regular expressions to find matches in the entire text
+    seeds = re.findall(r'seeds: ([\d\s]+)', input)
+    soil = re.findall(r'seed-to-soil map:\n([\d\s\n]+)', input)
+    fertilizer = re.findall(r'soil-to-fertilizer map:\n([\d\s\n]+)', input)
+    water = re.findall(r'fertilizer-to-water map:\n([\d\s\n]+)', input)
+    light = re.findall(r'water-to-light map:\n([\d\s\n]+)', input)
+    temperature = re.findall(r'light-to-temperature map:\n([\d\s\n]+)', input)
+    humidity = re.findall(r'temperature-to-humidity map:\n([\d\s\n]+)', input)
+    location = re.findall(r'humidity-to-location map:\n([\d\s\n]+)', input)
+
+    # Extract individual numbers from the captured groups
+    seeds = re.findall(r'\b\d+\b', ' '.join(seeds))
+    soil = [re.findall(r'\b\d+\b', line)
+            for line in soil[0].split('\n') if line.strip()]
+    fertilizer = [re.findall(r'\b\d+\b', line)
+                  for line in fertilizer[0].split('\n') if line.strip()]
+    water = [re.findall(r'\b\d+\b', line)
+             for line in water[0].split('\n') if line.strip()]
+    light = [re.findall(r'\b\d+\b', line)
+             for line in light[0].split('\n') if line.strip()]
+    temperature = [re.findall(r'\b\d+\b', line)
+                   for line in temperature[0].split('\n') if line.strip()]
+    humidity = [re.findall(r'\b\d+\b', line)
+                for line in humidity[0].split('\n') if line.strip()]
+    location = [re.findall(r'\b\d+\b', line)
+                for line in location[0].split('\n') if line.strip()]
+
+    return map(["100", "1700"], soil, fertilizer, water, light, temperature, humidity, location)
+    # Add similar print statements for other variables if needed
+
+
+print(ex(start()))
--- 2023/7a.py
+++ 2023/7a.py
@@ -1,14 +1,102 @@
 import re
+from collections import Counter
+
+from numpy import sort
+
+debug = False
+
 
 def start():
     with open('7.txt') as f:
         input = f.readlines()
     return input
 
+
+def calculate(finalList):
+    result = 0
+    for i, j in enumerate(finalList):
+        print(f"{int(i+1)} * {int(j)} = {int(i+1)*int(j)}") if debug == True else None
+        result += int(i+1)*int(j)
+    return result
+
+
+def ReOrder(hands):
+    Strength = ["A", "K", "Q", "J", "T", "9",
+                "8", "7", "6", "5", "4", "3", "2"]
+    converted = []
+    secondList = {}
+    kkkkk = 0
+    finalList = []
+    for i in hands.values():
+        if len(i) == 0:
+            continue
+        kkkkk += 1
+        for y in i:
+            print("y:", y.split(" ")[0]) if debug == True else None
+            value = y.split(" ")[1]
+            for z in y.split(" ")[0]:
+                converted.append(Strength.index(z))
+
+            print("converted", [l for l in converted]
+                  ) if debug == True else None
+            print("value", value) if debug == True else None
+
+            secondList[value+"_"+str(''.join([str(l) for l in converted]))
+                       ] = converted
+            converted = []
+        secondList = dict(
+            sorted(secondList.items(), key=lambda item: item[1]))
+        print(
+            f"{(list(hands.keys())[kkkkk-1])}: :{secondList}") if debug == True else None
+        finalList.append([roh.split("_")[0] for roh in secondList.keys()])
+        secondList = {}
+
+    return [s for s in finalList]
+
+
 def ex(input):
-    three = ""
-    print(input[-1])
-    three = re.findall(r'(?<!\w)(\w)(?!(?:.*\1){3})(?=(?:.*\1){2}.*\b\1\b).*$', input[-1])
-    return three
+    Five = []
+    Four = []
+    FullHouse = []
+    Three = []
+    Two = []
+    One = []
+    HighCard = []
+
+    for line in input:
+        line = line.strip("\n")
+        chars = re.findall(r'.', line.split(" ")[0])
+
+        counts = Counter(chars)
+        print([char for char in counts.values()]
+              ) if debug == True else None
+        if [char for char in counts.values()] == [5]:
+            Five.append(line)
+        elif [char for char in counts.values()] == [4, 1] or [char for char in counts.values()] == [1, 4]:
+            Four.append(line)
+        elif [char for char in counts.values()] == [3, 2] or [char for char in counts.values()] == [2, 3]:
+            FullHouse.append(line)
+        elif [char for char in counts.values()] == [3, 1, 1] or [char for char in counts.values()] == [1, 3, 1] or [char for char in counts.values()] == [1, 1, 3]:
+            Three.append(line)
+        elif [char for char in counts.values()] == [2, 2, 1] or [char for char in counts.values()] == [2, 1, 2] or [char for char in counts.values()] == [1, 2, 2]:
+            Two.append(line)
+        elif [char for char in counts.values()] == [2, 1, 1, 1] or [char for char in counts.values()] == [1, 2, 1, 1] or [char for char in counts.values()] == [1, 1, 2, 1] or [char for char in counts.values()] == [1, 1, 1, 2]:
+            One.append(line)
+        elif [char for char in counts.values()] == [1, 1, 1, 1, 1]:
+            HighCard.append(line)
+        else:
+            print("problem")
+    Hands = {"Five": Five, "Four": Four, "FullHouse": FullHouse,
+             "Three": Three, "Two": Two, "One": One, "HighCard": HighCard}
+
+    OneHand = ReOrder(Hands)
+
+    OneHand = [item for sublist in OneHand for item in sublist]
+    OneHand.reverse()
+
+    print(OneHand) if debug == True else None
+
+    return calculate(OneHand)
+
 
 print(ex(start()))
