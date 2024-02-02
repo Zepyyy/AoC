55d38216ed259b38d4b84b65a44d6ba2eb34180f
Zepyyy <quentin.stubecki@viacesi.fr>
2024-02-02 10:16:11 +0100

    13a

---
+++ 2023/13a.py
@@ -0,0 +1,17 @@
+def start():
+    with open("13.txt") as f:
+        out = f.read().strip().split("\n")
+    return out
+
+
+def getNextPattern(input):
+    return 0
+
+
+def ex(out):
+    pattern = 0
+
+    return pattern
+
+
+print(ex())
